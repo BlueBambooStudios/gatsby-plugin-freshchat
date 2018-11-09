91a3af916a9c39183c87ac2db06d3578aad8afef
Harry Walter <harry.walter@lqdinternet.com>
2018-11-09 11:43:41 +0000

    Initial Import

---
+++ src/gatsby-ssr.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+exports.onRenderBody = (
+    { setHeadComponents, setPostBodyComponents },
+    pluginOptions
+  ) => {
+    if (typeof pluginOptions.token === `undefined`) {
+        return null
+    }
+  
+    const freshchatHost = 
+      pluginOptions.host
+        ? pluginOptions.host
+        : `https://wchat.freshchat.com`
+  
+    const renderHTML = () => `
+      window.fcWidget.init({
+        token: "${pluginOptions.token}",
+        host: "${freshchatHost}",
+      })
+    `
+  
+    setHeadComponents([
+      <script
+        key={`gatsby-plugin-freshchat`}
+        src={`https://wchat.freshchat.com/js/widget.js`}
+      />
+    ])
+    setPostBodyComponents([
+      <script
+        key={`gatsby-plugin-freshchat-init`}
+        dangerouslySetInnerHTML={{ __html: renderHTML() }}
+      />
+    ])
+  }
+  
